18191bd190c791a0529ae0a3b4d799b12f31800f
admin <admin@app.lighting>
2020-05-05 20:26:38 -0400

    unlibrary index.js for debugging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express  = require('express');
-const applight = require('./libapplight.js');
+//const applight = require('./libapplight.js');
 const twilio   = require('twilio');
 const client   = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const VoiceResponse     = twilio.twiml.VoiceResponse;
@@ -8,6 +8,78 @@ const ClientCapability  = twilio.jwt.ClientCapability;
 const app               = express();
 
 const vaughan           = '+17818279675'
+
+
+
+// returns true if the person dialing in is a
+// registed app lighting client
+// isClient : String 'call endpoint' -> boolean
+function isClient( from ) {
+    // should query a database.. while in alpha/beta
+    // we'll just check the five values which should be true
+    if ( from === "+16173345281" || from === "+16173351304" ||
+	 from.startsWith("sim:") ||
+	 from === "sip:mvaughan@applight.sip.us1.twilio.com" )
+	return true;
+    else return false;
+}
+
+function clientPTSN( from ) {
+    // should query a database.. while in alpha/beta
+    // we'll just check the five values which should be true
+    switch ( from.trim() ) {
+    case "sim:DEdec7c449c69d576bd67a434bc92954e0":
+	return "+16173345281";
+	break;
+    case "sim:DEc4ad4e1e93c065c5e3df16a221d3c536":
+	return "+16173351304";
+	break;
+    case "sip:mvaughan@applight.sip.us1.twilio.com":
+	return "+19783879792";
+	break;
+	
+    }
+    // TODO: when relevant, add e164 checks here
+    return from;
+}
+
+// response for an App Lighting client
+function clientResponse( caller ) {
+    const response = new VoiceResponse();
+    const gather = response.gather({
+	input: 'speech dtmf',
+	numDigits: 1,
+	timeout: 7,
+	action: '/primary-client-choice',
+	method: 'POST'
+    });
+    gather.say('Welcome to App Lighting appointments and voicemail. '
+	       + 'To hear your messages, press one or say messages. '
+	       + 'To list your upcoming appointments, press two or '
+	       + 'say appointments. '
+	       + 'To begin or join a conference, press three or say '
+	       + 'conference. '
+	       + 'To enable or disable number proxies for your phone, '
+	       + 'press four or say proxies.' );
+    return response;
+}
+
+// response for all other callers
+function regularResponse() {
+    const response = new VoiceResponse();
+    const gather = response.gather({
+	input: 'speech dtmf',
+	numDigits: 1,
+	timeout: 7,
+	action: '/primary-choice',
+	method: 'POST'
+    });
+    gather.say('You have reached App Lighting. '
+	       + 'To schedule an appointment, press one or say appointment, '
+	       + 'To leave a message, press two or say message. ');
+    return response;
+}
+
 
 app.get('/voice-token', (request, response) => {
     const identity = 'the_user_id';
@@ -95,7 +167,8 @@ app.post('/call-avertest', (req, res) => {
 	to: '+16173990190',
 	record: 'true',
 	recordStatusCallback: 'https://comm.app.lighting/send-text-transcript',
-	twiml: '<Response><Dial callerId="+18882001601"><Number>' + vaughan + '</Number></Dial></Response>'
+	twiml: '<Response><Dial callerId="+18882001601"><Number>' + vaughan
+	    + '</Number></Dial></Response>'
     }).then( call => console.log(call.sid));
     
     res.writeHead(200, {'Content-Type': 'text/json'});
@@ -149,9 +222,9 @@ app.post('/primary-inbound', (req, res) => {
     if ( isClient(caller) ) {
 	// response = clientResponse( caller );
 	// while implementing regularResponse.. all callers are routed
-	response = applight.regularResponse();
+	response = regularResponse();
     } else {
-	response = applight.regularResponse();
+	response = regularResponse();
     }
 
     if ( response === undefined ) {
